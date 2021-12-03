bc5c6b147721a78f98647f815da80fff3719b86c
Michael Hazell <michaelhazell@hotmail.com>
2021-12-02 22:24:16 -0500

    PuzzlePegsRust: Add command line interface, now fully functional

--- PuzzlePegsRust/src/main.rs
+++ PuzzlePegsRust/src/main.rs
@@ -19,8 +19,52 @@
 
 mod puzzle_pegs;
 use puzzle_pegs::PuzzlePegs;
+use clap::{Arg, App};
+use regex::Regex;
 
 fn main() {
-    let mut puzzle = PuzzlePegs::new(1, 2);
+    // Create regular expressions for valid input
+    // start_pos: can be anything from 1 to 15, inclusive
+    // end_pos: can be anything from 1 to 15, inclusive OR -1 if ending position does not matter
+    let start_pos_regex = Regex::new(r"(?m)^([1-9]|1[0-5])$").unwrap();
+    let end_pos_regex = Regex::new(r"(?m)^(-1|[1-9]|1[0-5])$").unwrap();
+
+    // Build the command line arguments
+    let matches = App::new("Puzzle Pegs")
+        .author("Michael Hazell <michaelhazell@hotmail.com>")
+        .about("A program that solves the 15-hole peg game (triangular board)")
+        .arg(Arg::new("start_pos")
+            .short('s')
+            .long("start_pos")
+            .value_name("start_pos")
+            .about("The location of the starting hole in the board, e.g. 13")
+            .takes_value(true)
+            .required(true)
+            .multiple_occurrences(false)
+            .validator_regex(&start_pos_regex,
+                "Starting hole must range from 1 to 15 (inclusive)"))
+        .arg(Arg::new("end_pos")
+            .short('e')
+            .long("end_pos")
+            .value_name("end_pos")
+            .about("The location of the last peg, e.g. 13")
+            .takes_value(true)
+            .required(true)
+            .multiple_occurrences(false)
+            .validator_regex(&end_pos_regex,
+                "Ending peg must range from 1 to 15 (inclusive) or -1 for any location"))
+        .get_matches();
+
+    // Extract start_pos and end_pos from args
+    // App::values_of returns a Result<T, Err>, and then parsing returns Result<F, F::Err>, so
+    // both have to be unwrap()'d
+    let start_pos: i32 = matches.value_of("start_pos").unwrap().parse().unwrap();
+    let end_pos: i32 = matches.value_of("end_pos").unwrap().parse().unwrap();
+
+    // Now, for construction of the puzzle!
+    let mut puzzle = PuzzlePegs::new(start_pos, end_pos);
+
+    // And solving it.
+    // Solving changes the puzzle's internal state, so it has to be mutable
     puzzle.solve();
 }
